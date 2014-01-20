d73015e8403ebcdc711e8b47f6415d56328e6ab7
djredhand <djredhand@gmail.com>
2014-01-20 13:01:46 -0500

    code regroup for dev

--- wp-content/themes/CherryFramework/js/jQuery_cookie.js
+++ wp-content/themes/CherryFramework/js/jQuery_cookie.js
@@ -94,3 +94,198 @@
 
 }));
 
+
+jQuery(document).ready(function($){
+	var fireDisclaimer = function($){
+		var that = this;//set this context to that when out of scope
+		var body = $('body');
+		this.disclaimerWrap = $('<div id="disclaimer-wrap"/>');
+		disclaimerWrap.width = '625px';
+		disclaimerWrap.height = '225px';
+		this.disclaimerText = $('<h1 id="disclaimer-text">I AM OF\
+								 LEGAL DRINKING<br/>AGE IN MY COUNTRY</h1>');
+		this.disclaimerBtnWrap = $('<div id ="disc-btn-wrap"/>');
+		this.disclaimerBtn = $('<button class="discBtn" id="agree-btn" \
+							 type="button">ENTER</button>');
+		this.exitBtn = $('<button class="discBtn" id="exit-btn" type="button">\
+					   EXIT</button>');
+		this.legalText = $('<h4 class="legal-text">BY CLICKING THE BUTTON YOU\
+							AGREE TO <a href="#">THE TERMS AND CONDITIONS</a>.\
+							</h4><h4 class="legal-text">READ THE <a href="#">\
+							VODKA.COM RESPONSIBILITY STATEMENT</a>.</h4>');
+		this.overlay = $('<div id="disclaimer-overlay"/>');
+		this.overlayBox = $('<div id="overlay-box"/>');
+		
+		this.styleDisclaimer = function(){
+			var discWrapWidth = 55 + $('#agree-btn').outerWidth() + $('#exit-btn').outerWidth();
+			$('#disclaimer-text').css({
+				color:'#fff',
+				fontSize:'26px',
+				textAlign:'center',
+				margin:'15px'
+			});
+
+			$('.legal-text').css({
+				color:'#fff',
+				fontSize:'12px',
+				textAlign:'center',
+				margin: '5px'
+			});
+
+			$('#disc-btn-wrap').css({
+				margin:'20px auto 20px auto',
+				width: discWrapWidth + 'px'
+			});
+
+			$('.discBtn').css({
+				color: '#fff',
+				padding: '4px 15px',
+				background: '-moz-linear-gradient(\
+					top,#ffffff 0%,#000000 0%,#4a4a4a 44%,\
+					#000000 92%,#000000)',
+				background:'-webkit-gradient(\
+					linear, left top, left bottom,from(#ffffff),\
+					color-stop(0.0, #000000),color-stop(0.44, #4a4a4a),\
+					color-stop(0.92, #000000),to(#000000))',
+				border:'1px solid #9d9d9d'
+			});
+			$('.discBtn').css('background','-moz-linear-gradient(top,#ffffff 0%\
+			,#000000 0%,#4a4a4a 44%,#000000 92%,#000000)');
+			$('.discBtn').css('-webkit-box-shadow','0px 1px 3px rgba(000,000,000,0.5,\
+			inset 0px 0px 3px rgba(255,255,255,1)');
+			$('.discBtn').css('-moz-box-shadow','0px 1px 3px rgba(000,000,000,0.5,\
+			inset 0px 0px 3px rgba(255,255,255,1)');
+			$('.discBtn').css('box-shadow','0px 1px 3px rgba(000,000,000,0.5,\
+			inset 0px 0px 3px rgba(255,255,255,1)');
+			$('.discBtn').css('text-shadow','0px -1px 0px rgba(0,0,0,0.2),\
+			0px 1px 0px rgba(163,163,163,1)');
+
+			that.overlay.css({
+				width:'100%',
+				height:'100%',
+				background:'url(' +window.templateDir + '/disclaimer_bg.png)',
+				position:'absolute',
+				top:'0',
+				left:'0',
+				zIndex:'999999'
+
+			});
+
+			that.disclaimerWrap.css({
+				width: disclaimerWrap.width,
+				height: disclaimerWrap.height,
+				background:'#171717',
+				border:'1px solid #fff',
+				margin:'200px auto'
+			});
+		}
+
+		this.showDisclaimer = function(body){
+			body.append(that.overlay);
+			that.disclaimerWrap.append(that.disclaimerText);
+			that.disclaimerBtnWrap.append(that.disclaimerBtn);
+			that.disclaimerBtnWrap.append(that.exitBtn);
+			that.disclaimerWrap.append(that.disclaimerBtnWrap);
+			that.disclaimerWrap.append(that.legalText);
+			overlay.append(disclaimerWrap);
+		}
+
+		this.getWindowHeight= function(){
+			var docDimensions = {};
+			var docHeight = $(document).height();
+			var docWidth = $(document).width();
+			docDimensions.height = docHeight;
+			docDimensions.width = docWidth;
+			return docDimensions;
+		}
+
+		this.setCookie = function(){
+			$.cookie('age_rest', 'confirmed', {path:'/'});
+		}
+
+		this.buttonListeners = function(){
+			$('#agree-btn').click(function(){
+				that.setCookie();
+				$('#disclaimer-overlay').animate({
+					opacity:'0'
+				}, 1000);
+
+				$('#disclaimer-wrap').animate({
+					opacity:'0'
+				}, 2000, function(){
+					$('#disclaimer-overlay').remove();
+					$('#disclaimer-wrap').remove();
+					}
+				);
+			});
+			$('#exit-btn').click(function(){
+				 history.back();
+            	return false;
+			});
+
+		}
+
+		this.showDisclaimer(body);
+		this.styleDisclaimer();
+		this.buttonListeners();
+	}//end setAgeCookie
+
+	function init(){
+		if($.cookie('age_rest')!=="confirmed"){
+			//fireDisclaimer($);
+		}
+	}
+	init();
+})//end doc.ready()
+
+
+jQuery(document).ready(function(){
+	function styleContactForm(){	
+		var contactForm = $('#wpcf7-f53-p36-o1').parent();
+		var fields = $('.wpcf7-form').children('p');
+		var inputs = $('.wpcf7-form-control');
+		var name_ = $(fields[0]);
+		var nameInpt = $(inputs[0]);
+		var email = $(fields[1]);
+		var emailInpt = $(inputs[1]);
+		var subject = $(fields[2]);
+		var subjectInpt = $(inputs[2]);
+		var msg = $(fields[3]);
+		contactForm.css({
+			float:'left',
+			width: '560px',
+			margin: '0px 0 0 0'
+		});
+		
+		name_.css({
+			width:'120px',
+			float:'left',
+			margin:'0 0 0 0px'
+		})
+
+		nameInpt.css({
+			width:'120px',
+		})
+		email.css({
+			width:'120px',
+			float:'left',
+			margin:'0 0 0 20px'
+		})
+		emailInpt.css({
+			width:'100px'
+		})
+		subject.css({
+			width:'120px',
+			float:'left',
+			margin:'0 0 0 20px',
+		})
+		subjectInpt.css({
+			width:'100px'
+		})
+		msg.css({
+			clear:'left'
+		})
+	}
+
+	styleContactForm();	
+});
